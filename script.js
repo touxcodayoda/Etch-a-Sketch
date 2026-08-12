748216e137e910f356a3e89f2da83adcb5399972
touxcodayoda <prostroki123@gmail.com>
2026-08-12 23:30:57 +0800

    Added 256 divs using JS For Loops

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container")
+
+for(let i=0; i < 256; i++){
+    const content = document.createElement("div");
+    content.classList.add("content")
+    container.appendChild(content)
+}
+
+
+
+
